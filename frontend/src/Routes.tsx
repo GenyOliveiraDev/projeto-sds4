549d01c49696c1a51137a3be7107b9ec780b62a7
Geny Oliveira <mgao1961@gmail.com>
2021-09-12 10:35:38 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,21 @@
+import Dasboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/Dashboard">
+                    <Dasboard />
+                </Route>
+            </switch>
+
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
