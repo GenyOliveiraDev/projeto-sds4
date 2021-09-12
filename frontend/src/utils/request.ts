bfecfd5f1bbc4870f8df3f69cc605afc814acbd3
Geny Oliveira <mgao1961@gmail.com>
2021-09-12 20:31:16 -0300

    Env var

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,3 +1,3 @@
 import { env } from "process";
 
-export const BASE_URL = 'https://sds4-geny.herokuapp.com';
+export const BASE_URL = process env.REACT_APP_BACKEND_URL ?? 'https://sds4-geny.herokuapp.com';
