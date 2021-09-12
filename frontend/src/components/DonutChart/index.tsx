53efd0947cbbb9d68a9346f384d41dae20b844c3
Geny Oliveira <mgao1961@gmail.com>
2021-09-12 18:16:59 -0300

    DataTable integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -12,9 +12,6 @@ const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    //FORMA ERRADA - declarar a variavel aqui
-    //let chartData: ChartData = { labels: [], series: [] };
-
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
